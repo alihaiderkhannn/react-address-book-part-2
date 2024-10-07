a770bb8387ae017f22a4192a8ab944267ad08723
Ali Haider Khan <ali_786@live.no>
2024-10-07 13:33:30 +0200

    extensions

--- src/components/ContactDetail.jsx
+++ src/components/ContactDetail.jsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, } from "react";
+import { useNavigate } from "react-router-dom";
 import { ContactContext } from "../App";
 import { useParams } from "react-router-dom";
 
@@ -7,9 +8,11 @@ export default function ContactDetail() {
     const { contacts } = useContext(ContactContext);
     const [contact, setContact] = useState(null);
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         console.log(contacts)
+        console.log(id)
 
         if (contacts && id) {
 
@@ -25,6 +28,28 @@ export default function ContactDetail() {
 
 },  [contacts, id]);
 
+    const handleDelete = async () => {
+        try {
+          const response = await fetch(`https://boolean-uk-api-server.fly.dev/alihaiderkhannn/contact/${id}`, {
+                method: "DELETE",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if (!response.ok){
+                throw new Error('Failed while deleting the contact')
+            }
+
+            navigate('/contacts')
+            
+        } catch(error){
+            console.error("Error while deleting the contact", error)
+        }
+    }
+    
+
 if (!contact) return <p>Loading...</p>
 
 return (
@@ -32,6 +57,7 @@ return (
         <h3>Name: {contact.firstName} {contact.lastName}</h3>
         <p>Street: {contact.street}</p>
         <p>City: {contact.city}</p>
+        <button onClick={handleDelete}>Delete Contact</button>
     </div>
 )
 
